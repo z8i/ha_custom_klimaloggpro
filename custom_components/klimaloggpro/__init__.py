28f3a39113fe230eb147dcc5729358b6b9a3bd50
Martin <37486180+z8i@users.noreply.github.com>
2024-07-13 01:51:48 +0200

    async fixes

--- custom_components/klimaloggpro/__init__.py
+++ custom_components/klimaloggpro/__init__.py
@@ -31,6 +31,12 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up klimaloggpro from a config entry."""
     hass.data[DOMAIN][entry.entry_id] = entry.data # not sure, if this is needed...
     hass.data[DOMAIN]["kldr"] = kloggpro.klimalogg.KlimaLoggDriver() # creates an instance of the klimalogg-device
+    # Now we need to wait until the object is properly created, to call clear wait at start. But that turned out harder than expected.
+    
+    # while not hasattr(hass.data[DOMAIN]["kldr"], 'clear_wait_at_start'): # seems to never fulfill
+    # while hass.data[DOMAIN]["kldr"] is None: # also not working...
+
+    await asyncio.sleep(8) # puh, not nice but works somehow
     hass.data[DOMAIN]["kldr"].clear_wait_at_start() # necessary from the klimalogg-driver
     _LOGGER.info("Driver set up and started, push 'USB' Button on Logger now!")
 
